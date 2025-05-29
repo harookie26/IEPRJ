2afcb627afb13ae50e40784f6b07d8c2467c76f9
harookie <francis_m_obina@dlsu.edu.ph>
2025-05-29 22:10:24 +0800

    2D hiding mechanic
    add more stuff to emphasize where the player is hiding

--- Assets/Scripts/2D/Hiding_Script.cs
+++ Assets/Scripts/2D/Hiding_Script.cs
@@ -1,38 +1,84 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class Hiding_Script : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
+    [SerializeField] private float graceDistance = 0.2f; // Adjust as needed
+    [SerializeField] private List<GameObject> hidingSpotPrefabs; // Assign different prefabs in Inspector
+    [SerializeField] private List<Vector3> hidingSpotPositions;  // Assign positions in Inspector
 
-    GameObject protagonist = GameObject.FindGameObjectWithTag("Player");
-    [SerializeField] private float timer;
-    private bool isUsed;
+    private SpriteRenderer playerSpriteRenderer;
+    private List<GameObject> hidingSpots = new List<GameObject>();
 
+    private bool canHide = false;
+    private bool isHiding = false;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        isUsed = false;
+        for (int i = 0; i < hidingSpotPrefabs.Count && i < hidingSpotPositions.Count; i++)
+        {
+            GameObject spot = Instantiate(hidingSpotPrefabs[i], hidingSpotPositions[i], Quaternion.identity);
+            hidingSpots.Add(spot);
+        }
+
+        playerSpriteRenderer = player.GetComponent<SpriteRenderer>();
+
+        canHide = false;
+        isHiding = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CheckHidingSpot()
     {
-        if (isUsed == true)
+        canHide = false;
+        foreach (GameObject spot in hidingSpots)
         {
-            timer -= Time.deltaTime;
-
-            if (timer <= 0)
+            if (Mathf.Abs(spot.transform.position.x - player.transform.position.x) <= graceDistance)
             {
-                protagonist.SetActive(true);
-                Destroy(this);
+                canHide = true;
+                break;
             }
         }
     }
 
-    void OnHiding()
+    private void Update()
     {
-        protagonist.SetActive(false);
-        isUsed = true;
+        CheckHidingSpot();
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (canHide && !isHiding)
+            {
+                isHiding = true;
+                Debug.Log("Player is hiding");
+            }
+            else if (isHiding)
+            {
+                isHiding = false;
+                Debug.Log("Player stopped hiding");
+            }
+            else
+            {
+                Debug.Log("No hiding spot available");
+            }
+        }
+
+        if (isHiding)
+        {
+            player.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+            player.GetComponent<PlayerMovement2D>().enabled = false;
+            if (playerSpriteRenderer != null)
+                playerSpriteRenderer.enabled = false;
+        }
+        else
+        {
+            player.GetComponent<PlayerMovement2D>().enabled = true;
+            if (playerSpriteRenderer != null)
+                playerSpriteRenderer.enabled = true;
+        }
     }
+
+
 }
