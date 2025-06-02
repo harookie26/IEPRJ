a9ee748d229d6cbdd27f7eab98873277b504cd7e
harookie <francis_m_obina@dlsu.edu.ph>
2025-06-02 10:45:49 +0800

    3D room collision
    + adjusted vertical clamp of camera

--- Assets/Scripts/FirstPersonCam.cs
+++ Assets/Scripts/FirstPersonCam.cs
@@ -1,11 +1,13 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class FirstPersonCam : MonoBehaviour
 {
-    public Transform cameraTarget;
-    public float sensitivity = 2f;
-    public float verticalClamp = 85f;
+    [SerializeField] private Transform cameraTarget;
+    [SerializeField] private float sensitivity = 2f;
+    [SerializeField] private float verticalClamp = 70f;
+    [SerializeField] private float negVerticalClamp = -55f;
 
     private float yaw = 0f;
     private float pitch = 0f;
@@ -26,7 +28,7 @@ public class FirstPersonCam : MonoBehaviour
         Vector2 mouseDelta = Mouse.current.delta.ReadValue();
         yaw += mouseDelta.x * sensitivity;
         pitch -= mouseDelta.y * sensitivity;
-        pitch = Mathf.Clamp(pitch, -verticalClamp, verticalClamp);
+        pitch = Mathf.Clamp(pitch, negVerticalClamp, verticalClamp);
 
         this.gameObject.transform.localRotation = Quaternion.Euler(pitch, yaw, 0f);
     }
