7d0635a16395ecb8fd436a406ece32256f6e44c8
harookie <francis_m_obina@dlsu.edu.ph>
2025-07-19 20:44:47 +0800

    Add 2D camera and cutscene animation system
    Introduces new camera and cutscene animation controllers, states, and scripts for 2D gameplay. Adds camera idle, intro, and outro cutscene animations, cinematic bar transitions, and supporting scripts for cutscene event management and camera animation control. Updates related scenes and mechanics to integrate the new cutscene system.

---
+++ Assets/Scripts/2D/Cutscene/CutsceneEventFlag.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using static EventNames;
+
+public class CutsceneEventFlag : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the object that entered the trigger is on the "Player" layer.
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            EventBroadcaster.Instance.PostEvent(CutsceneEvents.CUTSCENE_START);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Check if the object that exited the trigger is on the "Player" layer.
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            EventBroadcaster.Instance.PostEvent(CutsceneEvents.CUTSCENE_END);
+        }
+    }
+}
