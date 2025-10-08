4eb43cc4c981947dd3fe2219946f97d180132151
Marlou Ruiz <marlou_ruiz@dlsu.edu.ph>
2025-10-09 02:33:30 +0800

    SFX for Restoration and Distraction

--- Assets/Scripts/Level/Objects/Interactables/PaintingInteractable.cs
+++ Assets/Scripts/Level/Objects/Interactables/PaintingInteractable.cs
@@ -4,6 +4,41 @@ using Game.ObjectTypes;
 [RequireComponent(typeof(Collider))]
 public class PaintingInteractable : MonoBehaviour, IInteractable
 {
+    private AudioSource sfxAudioSource;
+    private AudioList audioList;
+
+
+
+    /* When Awake is called it will find the SFX object with the 
+     SFX audio source component in the scene and store a reference to it.
+
+     This audio source is what will be used to play the SFX clips with 
+     the VolumeSettings script controlling the volume.
+    
+     Its important that each scene has a "Settings With Audio Manager Container"
+     as this prefab houses everything needed for audio management including the audio mixer 
+     and settings panel.
+
+     this prefab already includes the SFX and Music audio source object with the correct tag 
+     so as long this container is in every scene there should be no problem.
+     */
+    void Awake()
+    {
+        audioList = FindAnyObjectByType<AudioList>();
+        //Find the SFX audio source object in the scene by its tag
+        GameObject audioObject = GameObject.FindWithTag("SFXAudioSource");
+
+        if (audioObject != null)
+        {
+            sfxAudioSource = audioObject.GetComponent<AudioSource>();
+        }
+        else
+        {
+            Debug.LogWarning("No GameObject with tag 'SFXAudioSource' found in scene.");
+        }
+
+    }
+
     public void Interact()
     {
         // Find the enemy state machine in the scene and tell it to distract at this painting.
@@ -14,6 +49,7 @@ public class PaintingInteractable : MonoBehaviour, IInteractable
             return;
         }
 
+        sfxAudioSource.PlayOneShot(audioList.enemyDistractedSFX); //Play the SFX using the SFX audio source object reference
         enemyStateMachine.DistractAt(transform.position);
     }
 }
