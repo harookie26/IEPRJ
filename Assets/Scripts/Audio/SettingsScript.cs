af9c7f75c15ba952a890abe3f91bfdb7513488c3
Marlou Ruiz <marlou_ruiz@dlsu.edu.ph>
2025-10-22 03:15:26 +0800

    Settings Patch
    fixed cursor problem
    fixed button problem
    slight restructure on main menu buttons

--- Assets/Scripts/Audio/SettingsScript.cs
+++ Assets/Scripts/Audio/SettingsScript.cs
@@ -5,28 +5,24 @@ public class SettingsScript : MonoBehaviour
     [SerializeField] private GameObject settingsPanel;
     private bool isOpen = false;
 
-    void Update()
+    /*void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             ToggleSettings();
         }
-    }
+    }*/
 
-    public void onCloseButtonClicked()
-    {
-        ToggleSettings();
-    }
-
-    void ToggleSettings()
+    public void ToggleSettings()
     {
         isOpen = !isOpen;
         settingsPanel.SetActive(isOpen);
+        Debug.Log($"Toggling Settings Panel. Now open: {isOpen}");
 
         // Pause or unpause game
         //Time.timeScale = isOpen ? 0f : 1f;
 
-        if (isOpen)
+        /*if (isOpen)
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
@@ -35,6 +31,6 @@ public class SettingsScript : MonoBehaviour
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
-        }
+        }*/
     }
 }
