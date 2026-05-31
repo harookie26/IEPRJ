64a9bbaaebdb52f5e1dcb6bccf0b7ba7744fc685
Zion Obcena <zmobcena@gmail.com>
2026-05-31 13:17:28 +0800

    Add scripted encounters

--- Assets/Scripts/Level/EnemyIntroTrigger.cs
+++ Assets/Scripts/Level/EnemyIntroTrigger.cs
@@ -1,25 +1,31 @@
+using Game.States;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyIntroTrigger : MonoBehaviour
+public class EnemyIntroTrigger : MonoBehaviour, ISaveable
 {
     private AudioSource sfxAudioSource;
 
+    private static readonly Dictionary<string, EnemyIntroTrigger> Registry = new();
+
+    [Header("Identification")]
+    [SerializeField] private string uniqueID;
     public static EnemyIntroTrigger Instance { get; private set; }
 
     [SerializeField] private GameObject enemyIntroModel;
     [SerializeField] private AudioClip lightsOutSFX;
 
     public bool hasTriggered = false;
-
+    public string SaveKey => uniqueID;
     private void Awake()
     {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
+        //if (Instance != null && Instance != this)
+        //{
+        //    Destroy(gameObject);
+        //    return;
+        //}
 
-        Instance = this;
+        //Instance = this;
 
         GameObject audioObject1 = GameObject.FindWithTag("SFXAudioSource");
 
@@ -34,6 +40,13 @@ public class EnemyIntroTrigger : MonoBehaviour
             //.
             Debug.LogWarning("No GameObject with tag 'SFXAudioSource' found in scene.");
         }
+
+        Register();
+    }
+
+    private void OnDestroy()
+    {
+        Unregister();
     }
 
     private void Update()
@@ -52,6 +65,54 @@ public class EnemyIntroTrigger : MonoBehaviour
             DialogueTriggerManager.Instance.TriggerEnemyIntroDialogue();
             enemyIntroModel.SetActive(false);
             sfxAudioSource.Play();
+        }
+    }
+
+    private void Register()
+    {
+        if (string.IsNullOrWhiteSpace(uniqueID))
+        {
+            Debug.LogError($"EnemyIntroTrigger on {gameObject.name} has no unique ID.");
+            return;
+        }
+
+        if (Registry.ContainsKey(uniqueID))
+        {
+            Debug.LogError($"Duplicate EnemyIntroTrigger ID detected: {uniqueID}");
+            return;
+        }
+
+        Registry.Add(uniqueID, this);
+        GlobalSaveSystem.Register(this);
+    }
+
+    private void Unregister()
+    {
+        if (Registry.ContainsKey(uniqueID))
+            Registry.Remove(uniqueID);
+
+        GlobalSaveSystem.Unregister(this);
+    }
+
+    public object CaptureState()
+    {
+        return new EnemyIntroSaveData
+        {
+            hasTriggered = this.hasTriggered
+        };
+    }
+
+    public void RestoreState(object state)
+    {
+        if (state == null) return;
+
+        var data = (EnemyIntroSaveData)state;
+        this.hasTriggered = data.hasTriggered;
+
+        // Apply visual state immediately upon data restoration
+        if (this.hasTriggered && enemyIntroModel != null)
+        {
+            enemyIntroModel.SetActive(false);
         }
     }
 
