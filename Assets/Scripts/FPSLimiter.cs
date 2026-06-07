7fef60e21d26a3dd0657826db391147ea16d26d3
Zion Obcena <zmobcena@gmail.com>
2026-06-07 10:30:36 +0800

    Added signs, lighting

--- Assets/Scripts/FPSLimiter.cs
+++ Assets/Scripts/FPSLimiter.cs
@@ -5,9 +5,9 @@ public static class FPSLimiter
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void InitializeFPSCap()
     {
-        QualitySettings.vSyncCount = 0;
+        QualitySettings.vSyncCount = 1;
 
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = -1;
 
         Debug.Log("[FPSLimiter] Game frame rate capped to 60 FPS globally.");
     }
