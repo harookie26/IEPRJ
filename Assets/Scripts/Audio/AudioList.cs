767690e627562c2d0188f565fe13f7ab7413581b
harookie <francis_m_obina@dlsu.edu.ph>
2026-06-19 10:58:52 +0800

    Add UI SFX
    - Added new button click and SFX adjustment sounds with integration in AudioList and VolumeSettings for improved UI feedback.

--- Assets/Scripts/Audio/AudioList.cs
+++ Assets/Scripts/Audio/AudioList.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AudioList : MonoBehaviour
 {
+    public static AudioList Current { get; private set; }
+
     //Add here music clips and SFX clips to be used in the game
 
     [Header("Locked Door Clip")]
@@ -34,6 +38,12 @@ public class AudioList : MonoBehaviour
     [Header("Key Pickup SFX")]
     [SerializeField] public AudioClip keyPickupSFX;
 
+    [Header("Button Click SFX")]
+    [SerializeField] public AudioClip buttonClickSFX;
+
+    [Header("SFX Setting Adjustment")]
+    [SerializeField] public AudioClip sfxSettingAdjustment;
+
     [Header("Testing Clips")]
     [SerializeField] public AudioClip testMusic;
     [SerializeField] public AudioClip testSFX;
@@ -42,4 +52,88 @@ public class AudioList : MonoBehaviour
     [Tooltip("Dedicated AudioSource for restoration music.")]
     [SerializeField] public AudioSource restorationMusicAudioSource;
 
+    private AudioSource sfxAudioSource;
+
+    private void Awake()
+    {
+        Current = this;
+        ResolveSFXAudioSource();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void Start()
+    {
+        RegisterButtonClickSFX();
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        if (Current == this)
+        {
+            Current = null;
+        }
+    }
+
+    public void PlayButtonClickSFX()
+    {
+        PlaySFX(buttonClickSFX);
+    }
+
+    public void PlaySFXSettingAdjustment()
+    {
+        PlaySFX(sfxSettingAdjustment);
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        if (clip == null)
+        {
+            return;
+        }
+
+        ResolveSFXAudioSource();
+        if (sfxAudioSource != null)
+        {
+            sfxAudioSource.PlayOneShot(clip);
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        ResolveSFXAudioSource();
+        RegisterButtonClickSFX();
+    }
+
+    private void RegisterButtonClickSFX()
+    {
+        Button[] buttons = FindObjectsByType<Button>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        foreach (Button button in buttons)
+        {
+            button.onClick.RemoveListener(PlayButtonClickSFX);
+            button.onClick.AddListener(PlayButtonClickSFX);
+        }
+    }
+
+    private void ResolveSFXAudioSource()
+    {
+        if (sfxAudioSource != null)
+        {
+            return;
+        }
+
+        GameObject audioObject = GameObject.FindWithTag("SFXAudioSource");
+        if (audioObject != null)
+        {
+            sfxAudioSource = audioObject.GetComponent<AudioSource>();
+        }
+    }
 }
--- Assets/Scripts/Audio/VolumeSettings.cs
+++ Assets/Scripts/Audio/VolumeSettings.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
@@ -9,24 +10,26 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
 
+    private bool isAdjustingSFXSlider;
+
     public void SetMasterVolume()
     {
-        float masterVolume =masterSlider.value;
-        myMixer.SetFloat("MasterVolParam", Mathf.Log10(masterVolume) * 20);
+        float masterVolume = masterSlider.value;
+        myMixer.SetFloat("MasterVolParam", ToDecibels(masterVolume));
         PlayerPrefs.SetFloat("MasterVolume", masterVolume);
     }
 
     public void SetSFXVolume()
     {
         float sfxVolume = sfxSlider.value;
-        myMixer.SetFloat("SFXVolParam", Mathf.Log10(sfxVolume) * 20);
+        myMixer.SetFloat("SFXVolParam", ToDecibels(sfxVolume));
         PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
     }
 
     public void SetMusicVolume()
     {
         float musicVolume = musicSlider.value;
-        myMixer.SetFloat("MusicVolParam", Mathf.Log10(musicVolume) * 20);
+        myMixer.SetFloat("MusicVolParam", ToDecibels(musicVolume));
         PlayerPrefs.SetFloat("MusicVolume", musicVolume);
     }
 
@@ -45,6 +48,8 @@ public class VolumeSettings : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        RegisterSFXSliderReleaseSFX();
+
         if(PlayerPrefs.HasKey("MasterVolume") && PlayerPrefs.HasKey("SFXVolume") && PlayerPrefs.HasKey("MusicVolume"))
         {
             LoadVolume();
@@ -62,5 +67,54 @@ public class VolumeSettings : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void RegisterSFXSliderReleaseSFX()
+    {
+        if (sfxSlider == null)
+        {
+            return;
+        }
+
+        EventTrigger trigger = sfxSlider.GetComponent<EventTrigger>();
+        if (trigger == null)
+        {
+            trigger = sfxSlider.gameObject.AddComponent<EventTrigger>();
+        }
+
+        AddEventTriggerEntry(trigger, EventTriggerType.PointerDown, _ => isAdjustingSFXSlider = true);
+        AddEventTriggerEntry(trigger, EventTriggerType.PointerUp, _ => PlaySFXSliderReleaseSFX());
+        AddEventTriggerEntry(trigger, EventTriggerType.EndDrag, _ => PlaySFXSliderReleaseSFX());
+    }
+
+    private void PlaySFXSliderReleaseSFX()
+    {
+        if (!isAdjustingSFXSlider)
+        {
+            return;
+        }
+
+        isAdjustingSFXSlider = false;
+        SetSFXVolume();
+
+        if (AudioList.Current != null)
+        {
+            AudioList.Current.PlaySFXSettingAdjustment();
+        }
+    }
+
+    private void AddEventTriggerEntry(EventTrigger trigger, EventTriggerType type, UnityEngine.Events.UnityAction<BaseEventData> callback)
+    {
+        EventTrigger.Entry entry = new EventTrigger.Entry
+        {
+            eventID = type
+        };
+        entry.callback.AddListener(callback);
+        trigger.triggers.Add(entry);
+    }
+
+    private float ToDecibels(float volume)
+    {
+        return Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20;
     }
 }
